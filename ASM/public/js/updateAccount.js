1062603d5e2bbe4166235a50b1eec8a6e1003b9d
NgoDuy0810 <148345204+duynhpc05141@users.noreply.github.com>
2024-01-21 22:34:28 +0700

    ASM1_NgoHoangDuy_PC05141

--- ASM/public/js/updateAccount.js
+++ ASM/public/js/updateAccount.js
@@ -1,12 +1,62 @@
-const API_USER_UPDATE = 'http://localhost:3000/user';
-let btnSubmit = document.getElementById('btnSubmit');
+// const API_USER_UPDATE = 'http://localhost:3000/user';
+// let btnSubmit = document.getElementById('btnSubmit');
+// let urlParams = new URLSearchParams(window.location.search);
+// let id = urlParams.get('id');
+// console.log(id);
+
+// // Đoạn mã để thêm dữ liệu từ form vào cơ sở dữ liệu JSON
+// document.getElementById('btnSubmit').addEventListener('click', (e) => {
+//   e.preventDefault();
+//   let name = document.getElementById('name').value;
+//   let email = document.getElementById('email').value;
+//   let phone = document.getElementById('phone').value;
+//   let address = document.getElementById('address').value;
+//   let password = document.getElementById('pass').value;
+//   let role = document.getElementById('role').value;
+
+//   if (!name || !email || !phone || !address || !password || !role) {
+//     alert('Vui lòng điền đầy đủ thông tin');
+//     return;
+//   }
+
+//   let newData = {
+//     customer_name: name,
+//     customer_email: email,
+//     customer_phone_number: phone,
+//     customer_address: address,
+//     password: password,
+//     role_id: role
+//   };
+
+//   fetch(API_USER, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newData),
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     alert('Đăng ký thành công');
+//     window.location.href = '../../pages/login.html';
+//     // Thực hiện các hành động khác sau khi thêm dữ liệu thành công
+//   })
+//   .catch(error => {
+//     console.error('Lỗi khi thêm dữ liệu vào cơ sở dữ liệu:', error);
+//   });
+// });
+
+// Lấy thông tin người dùng từ URL query parameters
 let urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get('id');
 console.log(id);
-
-// Đoạn mã để thêm dữ liệu từ form vào cơ sở dữ liệu JSON
+const API_USER_UPDATE = 'http://localhost:3000/user/'+ id;
+console.log(API_USER_UPDATE);
+// Xử lý sự kiện khi người dùng nhấn nút "Submit"
 document.getElementById('btnSubmit').addEventListener('click', (e) => {
   e.preventDefault();
+
+  // Lấy thông tin từ các trường input
   let name = document.getElementById('name').value;
   let email = document.getElementById('email').value;
   let phone = document.getElementById('phone').value;
@@ -14,12 +64,14 @@ document.getElementById('btnSubmit').addEventListener('click', (e) => {
   let password = document.getElementById('pass').value;
   let role = document.getElementById('role').value;
 
+  // Kiểm tra xem các trường thông tin có được điền đầy đủ hay không
   if (!name || !email || !phone || !address || !password || !role) {
     alert('Vui lòng điền đầy đủ thông tin');
     return;
   }
 
-  let newData = {
+  // Dữ liệu mới cần cập nhật
+  let updatedData = {
     customer_name: name,
     customer_email: email,
     customer_phone_number: phone,
@@ -28,20 +80,29 @@ document.getElementById('btnSubmit').addEventListener('click', (e) => {
     role_id: role
   };
 
-  fetch(API_USER, {
-    method: 'POST',
+  // Gửi yêu cầu cập nhật thông tin người dùng lên cơ sở dữ liệu
+  fetch(API_USER_UPDATE, {
+    method: 'PUT', // Sử dụng phương thức PUT để cập nhật dữ liệu
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(newData),
+    body: JSON.stringify(updatedData),
   })
   .then(response => response.json())
   .then(data => {
-    alert('Đăng ký thành công');
-    window.location.href = '../../pages/login.html';
-    // Thực hiện các hành động khác sau khi thêm dữ liệu thành công
+    if (response.ok) {
+      sessionStorage.setItem('role_id', role_id);
+      sessionStorage.setItem('name', name);
+      sessionStorage.setItem('email', emailUser);
+      sessionStorage.setItem('pass', passUser);
+      sessionStorage.setItem('phone', phone);
+      sessionStorage.setItem('address', address);
+      sessionStorage.setItem('id', id);
+      alert('Cập nhật thông tin thành công');
+      window.location.href = '../../pages/userInfor.html?id=${id}'; // Chuyển hướng sau khi cập nhật thành công
+    }
   })
   .catch(error => {
-    console.error('Lỗi khi thêm dữ liệu vào cơ sở dữ liệu:', error);
+    console.error('Lỗi khi cập nhật thông tin:', error);
   });
 });
---
+++ ASM/public/js/userInfor.js
@@ -0,0 +1,53 @@
+let urlParams = new URLSearchParams(window.location.search);
+	let id = urlParams.get('id');
+	let UserRole = sessionStorage.getItem('role_id');
+	let role1 = `<div class="wrap-login100-form-btn my-xl-2 col-lg-6">
+	<div class="login100-form-bgbtn"></div>
+	<button class="login100-form-btn">
+		<a href="./updateUser.html?id=${id}" style="color: black;">Cập nhật tài khoản</a>
+	</button>
+	</div>
+		<div class="wrap-login100-form-btn my-xl-2 col-lg-6">
+	<div class="login100-form-bgbtn"></div>
+	<button class="login100-form-btn">
+		<a href="#" style="color: black;">Quản trị</a>
+	</button>
+	</div>`;
+	let role2 = `<div class="wrap-login100-form-btn my-xl-2 col-lg-6">
+	<div class="login100-form-bgbtn"></div>
+	<button class="login100-form-btn">
+		<a href="./updateUser.html?id=${id}" style="color: black;">Cập nhật tài khoản</a>
+	</button>
+	</div>`;
+	
+	// if (UserRole == 1) {
+	// 	return role1;
+	// }else{
+	// 	return role2;
+	// }
+	let roleId = (UserRole == 1) ? role1 : role2;
+
+	const API_USER_INFOR = 'http://localhost:3000/user/'+ id;
+	console.log(API_USER_INFOR);
+fetch(API_USER_INFOR)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let html = document.getElementById('userinfor');
+    let userInfor = "";
+	userInfor += `<span class="login100-form-title p-b-20" id="invalid">
+			<h3>Thông tin</h3>
+			${data.customer_name}
+		</span>
+		<div>
+			<p>Email: ${data.customer_email}<br>
+			Số điện thoại: ${data.customer_phone_number}<br>
+			Địa chỉ: ${data.customer_address}</p>
+		</div>
+
+		<div class="m-4" id="roleId">
+			${roleId}
+		</div>`;
+		html.innerHTML = userInfor;
+    });
