3cde696181fc5df4b668598fc6205d790a815fb6
NgoDuy0810 <148345204+duynhpc05141@users.noreply.github.com>
2024-01-26 15:10:43 +0700

    lab5_ngohoangduy_pc05141

--- Lab5/app.js
+++ Lab5/app.js
@@ -76,33 +76,133 @@ var persons = {
   
 
 // lab5 bai 5
+// class APICaller {
+//     constructor(baseURL) {
+//         this.baseURL = baseURL;
+//     }
+//     get(id ="" ){
+//         const API = this.baseURL + this.endpoint;
+//         fetch(API+id || "")
+//         .then((response) => response.json()
+//         .then((data) =>{ return console.log(data);})
+//         .catch((error) =>{ console.log('error');}));
+// }
+// }
+
+// class Comments extends APICaller {
+//     constructor(baseURL) {
+//         super(baseURL);
+//         this.endpoint = "comments/";
+//     }
+//     getAll(){
+//        return this.get();
+//     }
+
+//     getOne(id){
+//         return this.get(id);
+//     }
+// };
+// const API_URL = 'https://jsonplaceholder.typicode.com/';
+// let user = new Comments(API_URL);
+// user.getAll();
+// user.getOne(5);
 class APICaller {
-    constructor(baseURL) {
-        this.baseURL = baseURL;
-    }
-    get(id ="" ){
-        const API = this.baseURL + this.endpoint;
-        fetch(API+id || "")
-        .then((response) => response.json()
-        .then((data) =>{ return console.log(data);})
-        .catch((error) =>{ console.log('error');}));
-}
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  get(id = "") {
+    const API = this.baseURL + this.endpoint;
+    fetch(API + id || "")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log('error');
+      });
+  }
+
+  post(data) {
+    const API = this.baseURL + this.endpoint;
+    fetch(API, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log('error');
+      });
+  }
+
+  put(id, data) {
+    const API = this.baseURL + this.endpoint;
+    fetch(API + id, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log('error');
+      });
+  }
+
+  delete(id) {
+    const API = this.baseURL + this.endpoint;
+    fetch(API + id, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log('error');
+      });
+  }
 }
 
 class Comments extends APICaller {
-    constructor(baseURL) {
-        super(baseURL);
-        this.endpoint = "comments/";
-    }
-    getAll(){
-       return this.get();
-    }
+  constructor(baseURL) {
+    super(baseURL);
+    this.endpoint = "comments/";
+  }
 
-    getOne(id){
-        return this.get(id);
-    }
-};
+  getAll() {
+    return this.get();
+  }
+
+  getOne(id) {
+    return this.get(id);
+  }
+
+  addComment(data) {
+    return this.post(data);
+  }
+
+  updateComment(id, data) {
+    return this.put(id, data);
+  }
+
+  removeComment(id) {
+    return this.delete(id);
+  }
+}
+
 const API_URL = 'https://jsonplaceholder.typicode.com/';
 let user = new Comments(API_URL);
 user.getAll();
 user.getOne(5);
+
