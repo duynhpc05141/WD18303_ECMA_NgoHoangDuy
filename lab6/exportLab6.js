dc487770a8cac483e998344aea27255b45662a41
NgoDuy0810 <148345204+duynhpc05141@users.noreply.github.com>
2024-01-31 07:50:51 +0700

    lab6_ngohoangduy_pc05141

---
+++ lab6/exportLab6.js
@@ -0,0 +1,112 @@
+// lab6 bai 1/2
+export const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+export const lowercaseString = (string) => {
+    return string.toLowerCase();
+}
+
+// lab6 bai 3/4
+export default function subtract(x,y){
+    return x-y;
+}
+// lab6 bai 5
+export class APICaller {
+    constructor(baseURL) {
+      this.baseURL = baseURL;
+    }
+  
+    get(id = "") {
+      const API = this.baseURL + this.endpoint;
+      fetch(API + id || "")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log('error');
+        });
+    }
+  
+    post(data) {
+      const API = this.baseURL + this.endpoint;
+      fetch(API, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log('error');
+        });
+    }
+  
+    put(id, data) {
+      const API = this.baseURL + this.endpoint;
+      fetch(API + id, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log('error');
+        });
+    }
+  
+    delete(id) {
+      const API = this.baseURL + this.endpoint;
+      fetch(API + id, {
+        method: 'DELETE',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log('error');
+        });
+    }
+}
+
+export class Comments extends APICaller {
+constructor(baseURL) {
+        super(baseURL);
+        this.endpoint = "comments/";
+    }
+
+    getAll() {
+        return this.get();
+    }
+
+    getOne(id) {
+        return this.get(id);
+    }
+
+    addComment(data) {
+        return this.post(data);
+    }
+
+    updateComment(id, data) {
+        return this.put(id, data);
+    }
+
+    removeComment(id) {
+        return this.delete(id);
+    }
+}
+  
+export const API_URL = 'https://jsonplaceholder.typicode.com/';
+export let user = new Comments(API_URL);
+
---
+++ lab6/lab6.js
@@ -0,0 +1,13 @@
+import * as string from './exportLab6.js';
+console.log(string.uppercaseString('Hello World'));
+console.log(string.lowercaseString('Hello World'));
+
+// lab6 bai 4
+import subtract from './exportLab6.js';
+console.log(subtract(7,4));
+
+// lab6 bai 5
+import {user} from './exportLab6.js';
+user.getOne(6);
+user.getAll();
+user.addComment();
